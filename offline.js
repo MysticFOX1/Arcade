77a248430a3a0e886be4752ac83c0c16ad240c48
Mystic_FOX <99131751+MysticFOX1@users.noreply.github.com>
2023-02-11 00:22:12 +0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1676059206,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/laser-sheet1.png",
+		"images/mine-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/shadow-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/wall-sheet1.png",
+		"images/ballon-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
